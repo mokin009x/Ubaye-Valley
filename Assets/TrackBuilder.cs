422d75b29bc2eb25a35d1b366de18a8e0f0fe23b
Jaimy Magalhaes Coelho <mokin009x@hotmail.nl>
2018-04-08 16:39:43 +0200

    HidersBasicAi
    I Finalized the basic Ai of the hiders. in short the Hiders are in Run mode and run to theire randomly chosen hiding spot at the beginning. If they see the chaser while they run they go into Panic mode and their movement speed increases. when they reach there hiding spot they enter hiding mode and look around to see if the chaser is around.  when they see the chaser while in hiding mode first they enter Run mode to set new hiding spot and then enter panic mode automaticly to quickly escape.

---
+++ Assets/TrackBuilder.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.Experimental.UIElements;
+
+public class TrackBuilder : MonoBehaviour
+{
+    public GameObject startpiece;
+    public Button test;
+    public bool foundStartPiece;
+	// Use this for initialization
+    private void Awake()
+    {
+        foundStartPiece = false;
+    }
+
+    void Start () {
+       
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+	    GameObject startInScene = GameObject.Find("Track/StartPiece");
+
+        if (foundStartPiece == false && startInScene)
+        {
+            startpiece = startInScene;
+            foundStartPiece = true;
+
+            Debug.Log(foundStartPiece);
+	    }
+        else if (!startInScene)
+	    {
+	        Debug.Log(foundStartPiece);
+	    }
+
+	    
+	}
+
+
+}
