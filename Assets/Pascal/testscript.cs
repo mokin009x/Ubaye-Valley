ee1eb8c922c51bdf11118f9ecba81cb01a455a95
HoekigeHeer <ploxerdongaming@gmail.com>
2018-04-23 10:06:50 +0200

    Revert "Revert "Added FPS controller and BGM""
    This reverts commit 7ec70c05dfd953853617079a262ded5a996861ab.

--- Assets/Pascal/testscript.cs
+++ Assets/Pascal/testscript.cs
@@ -3,14 +3,18 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class testscript : MonoBehaviour {
-
+	float speed;
 	// Use this for initialization
 	void Start () {
-		
+		speed = 70f;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		transform.Translate(Input.acceleration.x, 0, -Input.acceleration.z);
+		transform.eulerAngles = new Vector3 (transform.localEulerAngles.x, transform.localEulerAngles.y + Input.acceleration.x * Time.deltaTime * speed, transform.localEulerAngles.z);
+	}
+
+	public void MoveForward(){
+		Debug.Log ("commit sudoku");
 	}
 }
